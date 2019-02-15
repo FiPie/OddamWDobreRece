a90c522af1dd9cc047d1e8d0c6d714825e735024
Filip Pie <filipiusz@gmail.com>
2019-02-15 12:16:38 +0100

    yet another tryout

---
+++ src/main/java/com/f/piechowiak/spring/OddamWDobreRece/OddamWDobreReceApplication.java
@@ -0,0 +1,14 @@
+package com.f.piechowiak.spring.OddamWDobreRece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddamWDobreReceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OddamWDobreReceApplication.class, args);
+	}
+
+}
+
